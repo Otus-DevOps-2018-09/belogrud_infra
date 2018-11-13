eb10aa14ade1add9ef399bc5db571f8c7b4d278e
Sergey Belogrud <belogrud@gmail.com>
2018-11-13 19:04:24 +0000

    Lesson 08 home work. Everything is here.

---
+++ terraform/variables.tf
@@ -0,0 +1,18 @@
+#variable project {
+#	description = "Project ID"
+#	project = "infra-219815"
+#}
+variable region {
+  description = "Region"
+  default     = "europe-west1"
+}
+
+#variable "public_key_path" {
+#	description = "Path to the public key used for ssh access"
+#	public_key_path = "~/.ssh/id_rsa_appuser.pub"
+#}
+#variable "disk_image" {
+#	description = "Disk image"
+#	disk_image = "reddit-base"
+#}
+
